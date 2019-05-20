805b89a2a61e416ce99ad1c6637d3bbb2ec4e9c2
Ankitagole <ankita17@navgurukul.org>
2019-05-20 06:36:44 +0530

    Add test case for sum product.

--- Test/java/strings/numberConvertor/ConvertorTest.java
+++ Test/java/strings/numberConvertor/ConvertorTest.java
@@ -2,6 +2,7 @@ package strings.numberConvertor;
 
 import main.java.strings.Convertor.Digit;
 import main.java.strings.Convertor.PowerSeries;
+import main.java.strings.Convertor.SumOfProduct;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -11,12 +12,17 @@ import static junit.framework.TestCase.assertEquals;
 
 public class ConvertorTest {
     @Test public void gettingNumberOfDigit() {
-        List<Integer> expect =  Arrays.asList(5,8,9);
-        assertEquals(expect, Digit.getDigits("985"));
+        List<Integer> expected =  Arrays.asList(5,8,9);
+        assertEquals(expected, Digit.getDigits("985"));
     }
     @Test public void gettingPowerOfSeries() {
-        List<Integer> expect =  Arrays.asList(1,8,64);
-        assertEquals(expect, PowerSeries.getPowerSeries(3,8));
+        List<Integer> expected =  Arrays.asList(1,8,64);
+        assertEquals(expected, PowerSeries.getPowerSeries(3,8));
+    }
+    @Test
+    public void gettingSumProduct() {
+        List<Integer> expected = Arrays.asList(2,10,12);
+        assertEquals(24, SumOfProduct.sumOfProduct(expected));
     }
 }
 
